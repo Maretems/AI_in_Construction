804f58a2835bba96ef0d8db42ed60e42a082cf43
Artem Moseev <45681457+Maretems@users.noreply.github.com>
2024-12-08 20:00:47 +0300

    Add files via upload

---
+++ Лабораторная работа 3/Поиск общих участников.py
@@ -0,0 +1,24 @@
+# TODO Напишите функцию find_common_participants
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+
+def find_common_participants(participants_first_group, participants_second_group, separator=','):
+    # Разделение по separator и перевод списков в множества
+    participants_first_group = set(participants_first_group.split(separator))
+    participants_second_group = set(participants_second_group.split(separator))
+
+    # Поиск пересечений между двумя множествами
+    common_participants = participants_first_group.intersection(participants_second_group)
+
+    # Перевод в список и сортировка значений по алфавиту
+    common_participants = sorted(list(common_participants))
+    return common_participants
+
+
+# Вывод результатов. Вручную вбил separator = '|', но в решении от составителей его оставляли
+# по умолчанию ','. Но в таком случае я получаю пустой список. Как быть?
+participants = find_common_participants(participants_first_group, participants_second_group, separator='|')
+print("Общие участники:", participants)
+
+# TODO Проверьте работу функции с разделителем отличным от запятой
